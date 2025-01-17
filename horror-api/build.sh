47b0871d3f00a5c5250c542557381d9be187bd15
Scott Gerring <scott@scottgerring.com>
2025-01-17 12:43:41 +0100

    Working, but with multiple docker invocations ...

--- horror-api/build.sh
+++ horror-api/build.sh
@@ -7,19 +7,24 @@ set -e
 # 
 
 # Build our amd64 and arm64 linux builds 
-docker buildx build --platform linux/arm64,linux/amd64 -t native-build .
+docker rm -f extract-native-build-arm64
+docker buildx build --platform linux/arm64 -t native-build-arm64 .
+docker create --name extract-native-build-arm64 native-build-arm64
+docker cp extract-native-build-arm64:/build/libtag_library.so ./output/libtag-linux-arm64.so
+docker rm -f extract-native-build-arm64
 
-# Extract the libraries
-docker create --name extract-native-build native-build
-docker cp extract-native-build:/output ./output
-docker rm extract-native-build
+docker rm -f extract-native-build-amd64
+docker buildx build --platform linux/amd64 -t native-build-amd64 .
+docker create --name extract-native-build-amd64 native-build-amd64
+docker cp extract-native-build-amd64:/build/libtag_library.so ./output/libtag-linux-amd64.so
+docker rm -f extract-native-build-amd64
 
 #
 # MacOS - ARM64 build
 #
 . ~/.sdkman/bin/sdkman-init.sh
 sdk use java 21-oracle
-gcc -shared -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" src/native-src/TagLibrary.c -o output/libtag-darwin-arm64.so
+gcc -shared -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin"  src/native-src/TagLibrary.c -o output/libtag-darwin-arm64.so
 
 #
 # Copy everything into the JARs tree for staging
